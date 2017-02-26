0a0e7f73720e273318e9ee45cf884946969f4aaf
Greg Allen <greg@firstandthird.com>
2017-02-26 15:03:51 -0800

    support for files as glob string

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+'use strict';
 const TaskkitTask = require('taskkit-task');
 const sourceMapExplorer = require('source-map-explorer');
 const path = require('path');
 const humanize = require('humanize');
+const glob = require('glob');
 
 class AnalyzerTask extends TaskkitTask {
+  execute(done) {
+    if (typeof this.options.files === 'string') {
+      glob(this.options.files, (err, files) => {
+        if (err) {
+          return done(err);
+        }
+        const map = {};
+        files.forEach((file) => {
+          map[file] = '';
+        });
+        this.options.files = map;
+        super.execute(done);
+      });
+      return;
+    }
+    super.execute(done);
+  }
+
   process(input, output, done) {
     const sourceMap = sourceMapExplorer.loadSourceMap(output);
     const data = sourceMapExplorer.computeGeneratedFileSizes(sourceMap.mapConsumer, sourceMap.jsData);
--- test/test.js
+++ test/test.js
@@ -1,10 +1,22 @@
+'use strict';
 const AnalyzeTask = require('../');
 
-const analyze = new AnalyzeTask('analyze', {
+const analyze1 = new AnalyzeTask('analyze', {
   files: {
     [`${__dirname}/dist.js`]: 'dummy'
   }
 });
-analyze.execute((err) => {
-  console.log(err);
+analyze1.execute((err) => {
+  if (err) {
+    throw err;
+  }
+});
+
+const analyze2 = new AnalyzeTask('analyze', {
+  files: `${__dirname}/di*.js`
+});
+analyze2.execute((err) => {
+  if (err) {
+    throw err;
+  }
 });
