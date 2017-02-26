4baafb7f8a41e120ecbb8c33bc8e1d9ccb90b62b
Greg Allen <greg@firstandthird.com>
2017-02-26 14:46:14 -0800

    fix using input vs output, humanize filesize

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const TaskkitTask = require('taskkit-task');
 const sourceMapExplorer = require('source-map-explorer');
 const path = require('path');
+const humanize = require('humanize');
 
 class AnalyzerTask extends TaskkitTask {
   process(input, output, done) {
-    const sourceMap = sourceMapExplorer.loadSourceMap(input);
+    const sourceMap = sourceMapExplorer.loadSourceMap(output);
     const data = sourceMapExplorer.computeGeneratedFileSizes(sourceMap.mapConsumer, sourceMap.jsData);
+    const map = {};
     Object.keys(data).forEach((file) => {
       const relativePath = path.relative(process.cwd(), file);
       const size = data[file];
-      this.log({
-        path: relativePath,
-        size
-      });
+      map[relativePath] = humanize.filesize(size);
     });
+    this.log(map);
     done();
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 const AnalyzeTask = require('../');
 
 const analyze = new AnalyzeTask('analyze', {
-  files: [
-    `${__dirname}/dist.js`
-  ]
+  files: {
+    [`${__dirname}/dist.js`]: 'dummy'
+  }
 });
 analyze.execute((err) => {
   console.log(err);
